d20cd98165829f1185d908512f29122ba3b2e9ee
Constantin <53481895+CostaVaryn@users.noreply.github.com>
2022-08-21 17:56:07 +0300

    Update WikiTest.java

--- src/test/java/wiki/WikiTest.java
+++ src/test/java/wiki/WikiTest.java
@@ -19,39 +19,39 @@ public class WikiTest extends BaseTest {
         List<String> links = new ArrayList<>();
         
         /** 
-            1 method:
-        for (int i = 0; i<hrefs.size(); i++){
-            links.add(hrefs.get(i).getAttribute("href"));
-        }
-            2 method:
-        for(SelenideElement element : hrefs){
-            links.add(element.getAttribute("href"));
-        }
-            3 method:
-        */
+         *     1 method:
+         * for (int i = 0; i<hrefs.size(); i++){
+         *     links.add(hrefs.get(i).getAttribute("href"));
+         * }
+         *     2 method:
+         * for(SelenideElement element : hrefs){
+         *     links.add(element.getAttribute("href"));
+         * }
+         *     3 method:
+         */
         
         hrefs.forEach(x->links.add(x.getAttribute("href")));
 
         /** 
-            1 способ: открытия всех полученных ссылок с помощью stream.api
-        links.forEach(Selenide::open);
-
-            2 способ: открытие всех ссылок и выполнение каких-либо assert'ов
-        for (int i = 0; i<links.size(); i++){
-            String listUrl = links.get(i);
-            Selenide.open(listUrl);
-            String currentUrl = WebDriverRunner.getWebDriver().getCurrentUrl();
-            Assert.assertEquals(currentUrl,listUrl);
-        }
-
-            3 способ: в рандомном порядке открыть все ссылки
-        Random random = new Random();
-        while (links.size() > 0) {
-            int randomNumber = random.nextInt(links.size());
-            Selenide.open(links.get(randomNumber));
-            links.remove(WebDriverRunner.getWebDriver().getCurrentUrl());
-        }
-        */
+         *    1 способ: открытия всех полученных ссылок с помощью stream.api
+         * links.forEach(Selenide::open);
+         *
+         *    2 способ: открытие всех ссылок и выполнение каких-либо assert'ов
+         * for (int i = 0; i<links.size(); i++){
+         *    String listUrl = links.get(i);
+         *    Selenide.open(listUrl);
+         *    String currentUrl = WebDriverRunner.getWebDriver().getCurrentUrl();
+         *    Assert.assertEquals(currentUrl,listUrl);
+         * }
+         *
+         *    3 способ: в рандомном порядке открыть все ссылки
+         * Random random = new Random();
+         * while (links.size() > 0) {
+         *    int randomNumber = random.nextInt(links.size());
+         *    Selenide.open(links.get(randomNumber));
+         *    links.remove(WebDriverRunner.getWebDriver().getCurrentUrl());
+         * }
+         */
 
         List<Integer> linkLength = hrefs.stream().map(x->x.getAttribute("href").length()).collect(Collectors.toList());
 
